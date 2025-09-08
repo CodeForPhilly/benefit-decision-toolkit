9435ecd7217883cedc6b4fb4166fae4ec8973008
Earth Walker <pineconefirestarter@pm.me>
2025-09-08 14:40:09 -0400

    use solidjs For to render lists instead of .map()

--- builder-frontend/src/components/project/TestsView.jsx
+++ builder-frontend/src/components/project/TestsView.jsx
@@ -1,4 +1,4 @@
-import { createSignal, Show } from "solid-js";
+import { createSignal, Show, For } from "solid-js";
 
 function TestsView() {
   const [expanded, setExpanded] = createSignal({});
@@ -63,43 +63,47 @@ function TestsView() {
           Run All
         </button>
       </div>
-      {tests.map((test) => (
-        <div className="border-1 rounded-lg px-4 p-2 mb-4">
-          <div className=" flex justify-between items-center">
-            <span>{test.name}</span>
-            <div className="flex gap-2">
-              <button className="border-1 rounded-lg border-green-500 bg-green-100 font-medium text-green-900 px-3 py-1">
-                Run
-              </button>
-              <button
-                className="border-1 border-gray-500 bg-gray-100 font-medium text-gray-900 px-3 py-1"
-                onClick={() => handleToggle(test.id)}
-              >
-                <Show when={!expanded()[test.id]} fallback="Hide">
-                  Show
-                </Show>
-              </button>
-            </div>
-          </div>
-          <Show when={expanded()[test.id]}>
-            <div className="py-2">
-              <div className="flex gap-2 justify-end">
-                <button className="border-1 rounded-lg border-gray-500 bg-gray-100 font-medium text-gray-900 px-3 py-1">
-                  Rename
+      <For each={tests}>
+        {(test) => (
+          <div className="border-1 rounded-lg px-4 p-2 mb-4">
+            <div className=" flex justify-between items-center">
+              <span>{test.name}</span>
+              <div className="flex gap-2">
+                <button className="border-1 rounded-lg border-green-500 bg-green-100 font-medium text-green-900 px-3 py-1">
+                  Run
                 </button>
-                <button className="border-1 border-red-500 bg-red-100 font-medium text-red-900 px-3 py-1">
-                  Delete
+                <button
+                  className="border-1 border-gray-500 bg-gray-100 font-medium text-gray-900 px-3 py-1"
+                  onClick={() => handleToggle(test.id)}
+                >
+                  <Show when={!expanded()[test.id]} fallback="Hide">
+                    Show
+                  </Show>
                 </button>
               </div>
-              {test.contents.map((contents) => (
-                <p>
-                  {contents.key}: {contents.value}
-                </p>
-              ))}
             </div>
-          </Show>
-        </div>
-      ))}
+            <Show when={expanded()[test.id]}>
+              <div className="py-2">
+                <div className="flex gap-2 justify-end">
+                  <button className="border-1 rounded-lg border-gray-500 bg-gray-100 font-medium text-gray-900 px-3 py-1">
+                    Rename
+                  </button>
+                  <button className="border-1 border-red-500 bg-red-100 font-medium text-red-900 px-3 py-1">
+                    Delete
+                  </button>
+                </div>
+                <For each={test.contents}>
+                  {(contents) => (
+                    <p>
+                      {contents.key}: {contents.value}
+                    </p>
+                  )}
+                </For>
+              </div>
+            </Show>
+          </div>
+        )}
+      </For>
     </div>
   );
 }
