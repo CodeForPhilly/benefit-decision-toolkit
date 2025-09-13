ebe275374119914ead2750b35318ecc582c4f889
Justin MacIntosh <jmacintosh@healthverity.com>
2025-09-13 13:06:09 -0400

    feat: TailwindCSS updates to Screener/Results

--- screener-frontend/src/Home.tsx
+++ screener-frontend/src/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div class="py-24 flex-col justify-center h-screen items-center">
-      <h1 class="text-center text-xl">Benefit Decision Toolkit</h1>
+      <h1 class="text-center text-[2em]">Benefit Decision Toolkit</h1>
     </div>
   );
 }
