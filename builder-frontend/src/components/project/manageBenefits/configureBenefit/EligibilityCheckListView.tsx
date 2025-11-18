c90934a2b9cd7e91d6e5da932b3ce1fcb83d1510
Michael Dratch <michael.s.dratch@gmail.com>
2025-11-17 21:02:00 -0500

    updated screener evaluation

--- builder-frontend/src/components/project/manageBenefits/configureBenefit/EligibilityCheckListView.tsx
+++ builder-frontend/src/components/project/manageBenefits/configureBenefit/EligibilityCheckListView.tsx
@@ -157,6 +157,7 @@ const EligibilityCheckRow = ({
           Add
         </div>
       </td>
+      <td class="eligibility-check-table-cell border-top">{check.name}</td>
       <td class="eligibility-check-table-cell border-top">
         {titleCase(check.name)}
       </td>
