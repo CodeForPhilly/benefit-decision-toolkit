db4a75ab50dd461a6ae0c6569b373c24077c629a
joshwanf <17016446+joshwanf@users.noreply.github.com>
2026-01-17 18:10:47 -0500

    added validation/serialization to Create Screener Endpoint

---
+++ builder-api/src/main/java/org/acme/api/error/UnknownFieldExceptionMapper.java
@@ -0,0 +1,25 @@
+package org.acme.api.error;
+
+import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import java.util.Map;
+
+// Global error handler for providing extra fields in the request body
+
+@Provider
+public class UnknownFieldExceptionMapper implements ExceptionMapper<UnrecognizedPropertyException> {
+
+    @Override
+    public Response toResponse(UnrecognizedPropertyException e) {
+        return Response.status(Response.Status.BAD_REQUEST)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(Map.of(
+                        "error", true,
+                        "message", "Unknown field " + e.getPropertyName()))
+                .build();
+    }
+}
