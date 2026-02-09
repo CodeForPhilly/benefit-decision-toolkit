5c6fd53cddabf6f364e8ec023e904b28657c5e02
joshwanf <17016446+joshwanf@users.noreply.github.com>
2026-02-09 13:43:12 -0500

    Removed Boostrap and configured Tailwind

--- builder-frontend/vite.config.js
+++ builder-frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [solid(), tsconfigPaths()],
+  plugins: [tailwindcss(), solid(), tsconfigPaths()],
   server: {
-    port: process.env.DEV_SERVER_PORT || 5173
-  }
+    port: process.env.DEV_SERVER_PORT || 5173,
+  },
 });
