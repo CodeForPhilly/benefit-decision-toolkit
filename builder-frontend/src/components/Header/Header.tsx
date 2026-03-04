097e38ade4b37d7aa602b640a2529667f8be1e7b
joshwanf <17016446+joshwanf@users.noreply.github.com>
2026-03-04 14:11:19 -0500

    Moved <Header> into it's own directory for subcomponents

--- builder-frontend/src/components/Header/Header.tsx
+++ builder-frontend/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 import { useLocation, useNavigate } from "@solidjs/router";
 
 import bdtLogo from "../assets/logos/bdt-logo-large-mono-light.svg";
