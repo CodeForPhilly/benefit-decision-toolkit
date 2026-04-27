4bbbba3316ad5b50169179b9cb48213e9abb348c
Earth Walker <pineconefirestarter@pm.me>
2026-04-27 11:15:03 -0400

    use p instead of div for confirmation modal explanation

--- builder-frontend/src/components/shared/ConfirmationModal.tsx
+++ builder-frontend/src/components/shared/ConfirmationModal.tsx
@@ -13,7 +13,7 @@ const ConfirmationModal = ({
     <div class="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
       <div class="bg-white px-12 py-8 rounded-xl max-w-140 w-1/2 min-w-80">
         <h2 class="text-2xl font-bold mb-3">{confirmationTitle}</h2>
-        <div class="mb-4">{confirmationText}</div>
+        <p class="mb-4">{confirmationText}</p>
         <div class="flex justify-end space-x-2">
           <button class="btn-default hover:bg-gray-200" onClick={closeModal}>
             Cancel
