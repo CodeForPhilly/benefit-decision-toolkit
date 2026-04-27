9b5d3938c173523ef1da61297163b6906b6dd51f
Earth Walker <pineconefirestarter@pm.me>
2026-04-27 11:14:08 -0400

    use p instead of div for deletion modal explanation

--- builder-frontend/src/components/homeScreen/DeleteConfirmation.tsx
+++ builder-frontend/src/components/homeScreen/DeleteConfirmation.tsx
@@ -13,9 +13,10 @@ export default function DeleteConfirmation(props: Props) {
       <h2 class="text-xl font-bold">
         Are you sure you would like to delete {props.screenerName}?
       </h2>
-      <div class="pt-8 text-md">
-        Once deleted, all associated data will be deleted and cant be recovered.
-      </div>
+      <p class="pt-8 text-md">
+        Once deleted, all associated data will be deleted and can't be
+        recovered.
+      </p>
       <div class="flex w-full justify-end gap-4 mt-8">
         <Button
           variant="outline-secondary"
